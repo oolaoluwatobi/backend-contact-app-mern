07f52ce4a752de34436335d4bb1ac7f2108e79a4
oolaoluwatobi <58662625+oolaoluwatobi@users.noreply.github.com>
2023-07-13 11:32:24 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,13 +1,9 @@
 const allowedOrigins = [
-  'https://www.google.com', 
-  'http://127.0.0.1:5500', 
-  'http://localhost:3500',
-  'http://localhost:3000',
   'http://localhost:5173',
   'http://localhost:5174',
-  'mongodb://localhost:27017',
   'https://contatcts-app-mern-client.vercel.app',
-  'https://64afaec5b08cad2f8daff42f--melodious-alpaca-0ee727.netlify.app'
+  'https://64afaec5b08cad2f8daff42f--melodious-alpaca-0ee727.netlify.app',
+  'https://main--melodious-alpaca-0ee727.netlify.app'
 ];
 
 module.exports = allowedOrigins
